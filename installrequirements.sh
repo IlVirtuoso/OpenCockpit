e037febb0625483aab85f321346b9f918391c11c
ImSans <matteo.ielacqua@edu.unito.it>
2021-12-21 19:09:14 +0100

    assembled project

--- installrequirements.sh
+++ installrequirements.sh
@@ -3,4 +3,4 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-apt install libboost-all-dev ninja-build libprotobuf-dev libusb-1.0-0-dev openssl cmake  libqt5-core  protobuf-compiler gcc-11 g++-11
+apt install libboost-all-dev libusb-1.0.0-dev libssl-dev cmake libprotobuf-dev protobuf-c-compiler protobuf-compiler libqt5multimedia5 libqt5multimedia5-plugins libqt5multimediawidgets5 qtmultimedia5-dev libqt5bluetooth5 libqt5bluetooth5-bin qtconnectivity5-dev pulseaudio librtaudio-dev  qtdeclarative5-dev
